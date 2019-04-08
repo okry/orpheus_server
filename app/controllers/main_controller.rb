c4cf8a3ba2fef1a5d973a77b0324979ea87bd155
okry <alexanderlokros@gmail.com>
2019-04-08 17:31:48 -0400

    started file i/o

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
 
+  # PWD is project root: /orpheusapp
+
   def index
   end
 
@@ -9,17 +11,22 @@ class MainController < ApplicationController
   def new
   end
 
-
+  def get_pwd
+    puts `echo $PWD`
+  end
+  helper_method :get_pwd
 
 
   def serve_song
     send_file '/home/okry/Documents/nyProjects/okry-temp.wav', type: 'audio.wav', disposition: 'inline'
   end
 
-  private
-
-  def generate_song
-
+  def generate_song(input_string)
+    puts input_string
+    `echo testing >> ./tmp/test.txt`
+    # exec "echo " + input_string + " | ny"
   end
+  helper_method :generate_song
+
 end
 
